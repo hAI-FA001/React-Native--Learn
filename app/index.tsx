7704f24faeeb264ef3666574296111e872a7af15
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-16 20:43:37 +0500

    ScrollView

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,40 @@
-import { Image, ImageBackground, Text, View } from "react-native";
+import { Image, ImageBackground, ScrollView, Text, View } from "react-native";
 
 const logoImg = require("@/assets/images/adaptive-icon.png");
 
 export default function Index() {
   return (
-    <View style={{ backgroundColor: "plum", flex: 1, padding: 60 }}>
-      {/* <Image source={logoImg} style={{ width: 300, height: 300 }} />
+    <ScrollView style={{ backgroundColor: "plum", flex: 1 }}>
       <Image
         source={{ uri: "https://picsum.photos/200" }}
         style={{ width: 200, height: 200 }}
-      /> */}
-      <ImageBackground source={logoImg} style={{ flex: 1 }}>
-        <Text>Hello, world!</Text>
-      </ImageBackground>
-    </View>
+      />
+      <Text>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+        repudiandae corrupti tenetur aut. Error aliquam quas itaque laboriosam
+        alias sapiente porro! Omnis dolorum ab in dolorem incidunt veniam,
+        corporis repudiandae. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Illum repudiandae corrupti tenetur aut. Error aliquam quas itaque
+        laboriosam alias sapiente porro! Omnis dolorum ab in dolorem incidunt
+        veniam, corporis repudiandae. Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Illum repudiandae corrupti tenetur aut. Error aliquam
+        quas itaque laboriosam alias sapiente porro! Omnis dolorum ab in dolorem
+        incidunt veniam, corporis repudiandae. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Illum repudiandae corrupti tenetur aut.
+        Error aliquam quas itaque laboriosam alias sapiente porro! Omnis dolorum
+        ab in dolorem incidunt veniam, corporis repudiandae. Lorem ipsum dolor
+        sit amet consectetur adipisicing elit. Illum repudiandae corrupti
+        tenetur aut. Error aliquam quas itaque laboriosam alias sapiente porro!
+        Omnis dolorum ab in dolorem incidunt veniam, corporis repudiandae. Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Illum repudiandae
+        corrupti tenetur aut. Error aliquam quas itaque laboriosam alias
+        sapiente porro! Omnis dolorum ab in dolorem incidunt veniam, corporis
+        repudiandae.
+      </Text>
+      <Image
+        source={{ uri: "https://picsum.photos/200" }}
+        style={{ width: 200, height: 200 }}
+      />
+    </ScrollView>
   );
 }
