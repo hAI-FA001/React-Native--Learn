ebb245cf09bd127384b48d7c3e95621460d4d0c6
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-16 21:20:41 +0500

    Alert API - title, msg, buttons

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import {
   ActivityIndicator,
+  Alert,
   Button,
   Image,
   ImageBackground,
@@ -13,29 +14,45 @@ import {
 } from "react-native";
 
 const logoImg = require("@/assets/images/adaptive-icon.png");
+const loremTxt =
+  "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel ipsa ea excepturi velit mollitia voluptates, laborum ducimus explicabo quibusdam quam aperiam ipsum. Vero repellat esse cupiditate inventore similique fugit et.";
 
 export default function Index() {
   const [isVis, setVis] = useState(false);
 
   return (
-    <View style={{ backgroundColor: "plum", flex: 1, padding: 60 }}>
-      <ActivityIndicator animating={isVis} />
-      <ActivityIndicator size={"small"} animating={isVis} />
-      <ActivityIndicator
-        size={"large"}
-        color={"midnightblue"}
-        animating={isVis} // show or hide
-      />
-      <ActivityIndicator
-        size={"large"}
-        color={"midnightblue"}
-        animating={isVis}
+    <View style={{ backgroundColor: "teal", flex: 1, padding: 60 }}>
+      <Button
+        title="Show 1"
+        onPress={() => {
+          Alert.alert("Hello, world!");
+        }}
       />
 
       <Button
-        title="Toggle Indicators"
+        title="Show 2"
         onPress={() => {
-          setVis(!isVis);
+          Alert.alert("Hello, world!", loremTxt);
+        }}
+      />
+
+      <Button
+        title="Show 3"
+        onPress={() => {
+          Alert.alert("Hello, world!", loremTxt, [
+            {
+              text: "OK",
+              onPress: () => {
+                console.log("OK pressed");
+              },
+            },
+            {
+              text: "Cancel",
+              onPress: () => {
+                console.log("Cancelled");
+              },
+            },
+          ]);
         }}
       />
     </View>
