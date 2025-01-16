e7ab83e07511750d6ee6d3378eedbe40c8bbc6b0
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-16 23:13:44 +0500

    flexDirection

--- Layout/App.js
+++ Layout/App.js
@@ -7,10 +7,11 @@ export default function App() {
     // "flex: 1" to occupy all space
     // <View style={{ backgroundColor: 'plum', flex: 1 }}></View>
     <View style={styles.container}>
-      <Box style={{ backgroundColor: '#8e9b00', flex: 1 }}>Box 1</Box>
-      <Box style={{ backgroundColor: '#b65d1f', flex: 3 }}>Box 2</Box>
+      <Box style={{ backgroundColor: '#8e9b00' }}>Box 1</Box>
+      <Box style={{ backgroundColor: '#b65d1f' }}>Box 2</Box>
       <Box style={{ backgroundColor: '#1c4c56' }}>Box 3</Box>
       <Box style={{ backgroundColor: '#ab9156' }}>Box 4</Box>
+      {/* rest of boxes overflow */}
       <Box style={{ backgroundColor: '#6b0003' }}>Box 5</Box>
       <Box style={{ backgroundColor: '#1c4c56' }}>Box 6</Box>
       <Box style={{ backgroundColor: '#b95f21' }}>Box 7</Box>
@@ -24,5 +25,6 @@ const styles = StyleSheet.create({
     borderWidth: 6,
     borderColor: 'red',
     flex: 1, // border shows it takes up all space rather than only space occupied by children
+    flexDirection: 'row',
   },
 })
