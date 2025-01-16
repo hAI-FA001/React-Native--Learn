9aafc29e08ac259e69d3689fb014e9020761ae85
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-16 21:52:33 +0500

    StyleSheet API

--- Styling/App.js
+++ Styling/App.js
@@ -1,5 +1,14 @@
-import { Text, View } from "react-native"
+import { StyleSheet, Text, View } from "react-native"
 
 export default App = () => {
-  return <View><Text>StyleSheet API</Text></View>
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>StyleSheet API</Text>
+    </View>
+  )
 }
+
+export const styles = StyleSheet.create({
+  container: { flex: 1, padding: 60, backgroundColor: "teal", },
+  title: { fontSize: 20 }
+})
