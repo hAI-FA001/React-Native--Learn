f074f72a4a6cfa28220ce24703fc3f7ae022b4d0
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-16 23:46:41 +0500

    flexBasis + comparison with height/width (distributed spacing considered by flexBasis but not height/width)

--- Layout/App.js
+++ Layout/App.js
@@ -7,16 +7,13 @@ export default function App() {
     // "flex: 1" to occupy all space
     // <View style={{ backgroundColor: 'plum', flex: 1 }}></View>
     <View style={styles.container}>
-      <Box style={{ backgroundColor: '#8e9b00', alignSelf: 'flex-start' }}>
-        Box 1
-      </Box>
-      <Box style={{ backgroundColor: '#b65d1f', alignSelf: 'center' }}>
-        Box 2
-      </Box>
-      <Box style={{ backgroundColor: '#1c4c56', alignSelf: 'flex-end' }}>
+      <Box style={{ backgroundColor: '#8e9b00' }}>Box 1</Box>
+      <Box style={{ backgroundColor: '#b65d1f' }}>Box 2</Box>
+      <Box style={{ backgroundColor: '#1c4c56', flexBasis: 100, flex: 1 }}>
         Box 3
       </Box>
-      <Box style={{ backgroundColor: '#ab9156' }}>Box 4</Box>
+      {/* height doesn't take into account distributed spacing, so this box is smaller */}
+      <Box style={{ backgroundColor: '#ab9156', height: 140 }}>Box 4</Box>
       {/* rest of boxes overflow */}
       <Box style={{ backgroundColor: '#6b0003' }}>Box 5</Box>
       <Box style={{ backgroundColor: '#1c4c56' }}>Box 6</Box>
@@ -31,17 +28,17 @@ const styles = StyleSheet.create({
     borderWidth: 6,
     borderColor: 'red',
 
-    height: 300,
+    // height: 300,
 
-    // flex: 1, // border shows it takes up all space rather than only space occupied by children
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'stretch',
-    flexWrap: 'wrap',
-    alignContent: 'center',
+    flex: 1, // border shows it takes up all space rather than only space occupied by children
+    // flexDirection: 'row',
+    // justifyContent: 'space-between',
+    // alignItems: 'flex-start',
+    // flexWrap: 'wrap',
+    // alignContent: 'center',
 
     // rowGap: 10,
     // columnGap: 20,
-    gap: 20,
+    // gap: 20,
   },
 })
