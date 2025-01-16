d446f41eeeb846f48e9e43e715fc87816176a04a
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-16 20:48:45 +0500

    <Button>, title, onPress, color, disabled

--- app/index.tsx
+++ app/index.tsx
@@ -1,43 +1,25 @@
-import { Image, ImageBackground, ScrollView, Text, View } from "react-native";
+import {
+  Button,
+  Image,
+  ImageBackground,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
 
 const logoImg = require("@/assets/images/adaptive-icon.png");
 
 export default function Index() {
   return (
     <View style={{ backgroundColor: "plum", flex: 1, padding: 60 }}>
-      <ScrollView>
-        <Image
-          source={{ uri: "https://picsum.photos/200" }}
-          style={{ width: 200, height: 200 }}
-        />
-        <Text>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
-          repudiandae corrupti tenetur aut. Error aliquam quas itaque laboriosam
-          alias sapiente porro! Omnis dolorum ab in dolorem incidunt veniam,
-          corporis repudiandae. Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Illum repudiandae corrupti tenetur aut. Error
-          aliquam quas itaque laboriosam alias sapiente porro! Omnis dolorum ab
-          in dolorem incidunt veniam, corporis repudiandae. Lorem ipsum dolor
-          sit amet consectetur adipisicing elit. Illum repudiandae corrupti
-          tenetur aut. Error aliquam quas itaque laboriosam alias sapiente
-          porro! Omnis dolorum ab in dolorem incidunt veniam, corporis
-          repudiandae. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Illum repudiandae corrupti tenetur aut. Error aliquam quas itaque
-          laboriosam alias sapiente porro! Omnis dolorum ab in dolorem incidunt
-          veniam, corporis repudiandae. Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Illum repudiandae corrupti tenetur aut. Error
-          aliquam quas itaque laboriosam alias sapiente porro! Omnis dolorum ab
-          in dolorem incidunt veniam, corporis repudiandae. Lorem ipsum dolor
-          sit amet consectetur adipisicing elit. Illum repudiandae corrupti
-          tenetur aut. Error aliquam quas itaque laboriosam alias sapiente
-          porro! Omnis dolorum ab in dolorem incidunt veniam, corporis
-          repudiandae.
-        </Text>
-        <Image
-          source={{ uri: "https://picsum.photos/200" }}
-          style={{ width: 200, height: 200 }}
-        />
-      </ScrollView>
+      <Button
+        title="Hello There"
+        onPress={() => {
+          console.log("Pressed button");
+        }}
+        color="midnightblue"
+        disabled
+      />
     </View>
   );
 }
