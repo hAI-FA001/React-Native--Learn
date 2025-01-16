ebc52030d00b5db8046e32d33e1a6e94b7f184e6
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-16 21:58:11 +0500

    StyleSheet API, box example

--- Styling/App.js
+++ Styling/App.js
@@ -1,14 +1,30 @@
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, View } from 'react-native'
 
 export default App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>StyleSheet API</Text>
+      <View style={styles.boxStyleA}>
+        <Text>Box 1</Text>
+      </View>
+      <View style={styles.boxStyleB}>
+        <Text>Box 2</Text>
+      </View>
     </View>
   )
 }
 
 export const styles = StyleSheet.create({
-  container: { flex: 1, padding: 60, backgroundColor: "teal", },
-  title: { fontSize: 20 }
+  container: { flex: 1, padding: 60, backgroundColor: 'teal' },
+  boxStyleA: {
+    backgroundColor: 'lightgreen',
+    width: 100,
+    height: 100,
+    padding: 10,
+  },
+  boxStyleB: {
+    backgroundColor: 'lightblue',
+    width: 100,
+    height: 100,
+    padding: 10,
+  },
 })
