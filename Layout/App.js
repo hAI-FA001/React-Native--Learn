b00c4d27264f4334a117f88440a8eedf505492e9
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-16 23:16:03 +0500

    flexbox - justifyContent

--- Layout/App.js
+++ Layout/App.js
@@ -10,11 +10,11 @@ export default function App() {
       <Box style={{ backgroundColor: '#8e9b00' }}>Box 1</Box>
       <Box style={{ backgroundColor: '#b65d1f' }}>Box 2</Box>
       <Box style={{ backgroundColor: '#1c4c56' }}>Box 3</Box>
-      <Box style={{ backgroundColor: '#ab9156' }}>Box 4</Box>
+      {/* <Box style={{ backgroundColor: '#ab9156' }}>Box 4</Box> */}
       {/* rest of boxes overflow */}
-      <Box style={{ backgroundColor: '#6b0003' }}>Box 5</Box>
+      {/* <Box style={{ backgroundColor: '#6b0003' }}>Box 5</Box>
       <Box style={{ backgroundColor: '#1c4c56' }}>Box 6</Box>
-      <Box style={{ backgroundColor: '#b95f21' }}>Box 7</Box>
+      <Box style={{ backgroundColor: '#b95f21' }}>Box 7</Box> */}
     </View>
   )
 }
@@ -24,7 +24,9 @@ const styles = StyleSheet.create({
     marginTop: 64,
     borderWidth: 6,
     borderColor: 'red',
+
     flex: 1, // border shows it takes up all space rather than only space occupied by children
-    flexDirection: 'row',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
   },
 })
