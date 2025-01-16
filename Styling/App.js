3e1e1c27da78c9103ff25e6816cda9cf5e86b16b
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-16 22:13:13 +0500

    box shadow (only works in iOS) + elevation (for Android) + shadowColor (works on both)

--- Styling/App.js
+++ Styling/App.js
@@ -3,11 +3,11 @@ import { StyleSheet, Text, View } from 'react-native'
 export default App = () => {
   return (
     <View style={styles.container}>
-      <View style={[styles.box, styles.lightGreen]}>
+      <View style={[styles.box, styles.lightGreen, styles.boxShadow]}>
         {/* borderRadius on Text only works on Android */}
         <Text style={{ backgroundColor: 'red', borderRadius: 5 }}>Box 1</Text>
       </View>
-      <View style={[styles.box, styles.lightBlue]}>
+      <View style={[styles.box, styles.lightBlue, styles.androidShadow]}>
         <Text>Box 2</Text>
       </View>
     </View>
@@ -36,5 +36,18 @@ export const styles = StyleSheet.create({
 
   lightBlue: {
     backgroundColor: 'lightblue',
+  },
+
+  boxShadow: {
+    // only applied in iOS
+    shadowColor: '#333333',
+    shadowOffset: { width: 6, height: 6 },
+    shadowOpacity: 0.6,
+    shadowRadius: 4,
+  },
+
+  androidShadow: {
+    elevation: 10,
+    shadowColor: 'blue', // only this works on both
   },
 })
