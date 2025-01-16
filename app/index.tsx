c1069671e3cfbd8e86165fb4222c384a7b0650d7
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-16 20:44:32 +0500

    wrap ScrollView within padded View to avoid overlap with status bar

--- app/index.tsx
+++ app/index.tsx
@@ -4,37 +4,40 @@ const logoImg = require("@/assets/images/adaptive-icon.png");
 
 export default function Index() {
   return (
-    <ScrollView style={{ backgroundColor: "plum", flex: 1 }}>
-      <Image
-        source={{ uri: "https://picsum.photos/200" }}
-        style={{ width: 200, height: 200 }}
-      />
-      <Text>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
-        repudiandae corrupti tenetur aut. Error aliquam quas itaque laboriosam
-        alias sapiente porro! Omnis dolorum ab in dolorem incidunt veniam,
-        corporis repudiandae. Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Illum repudiandae corrupti tenetur aut. Error aliquam quas itaque
-        laboriosam alias sapiente porro! Omnis dolorum ab in dolorem incidunt
-        veniam, corporis repudiandae. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Illum repudiandae corrupti tenetur aut. Error aliquam
-        quas itaque laboriosam alias sapiente porro! Omnis dolorum ab in dolorem
-        incidunt veniam, corporis repudiandae. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Illum repudiandae corrupti tenetur aut.
-        Error aliquam quas itaque laboriosam alias sapiente porro! Omnis dolorum
-        ab in dolorem incidunt veniam, corporis repudiandae. Lorem ipsum dolor
-        sit amet consectetur adipisicing elit. Illum repudiandae corrupti
-        tenetur aut. Error aliquam quas itaque laboriosam alias sapiente porro!
-        Omnis dolorum ab in dolorem incidunt veniam, corporis repudiandae. Lorem
-        ipsum dolor sit amet consectetur adipisicing elit. Illum repudiandae
-        corrupti tenetur aut. Error aliquam quas itaque laboriosam alias
-        sapiente porro! Omnis dolorum ab in dolorem incidunt veniam, corporis
-        repudiandae.
-      </Text>
-      <Image
-        source={{ uri: "https://picsum.photos/200" }}
-        style={{ width: 200, height: 200 }}
-      />
-    </ScrollView>
+    <View style={{ backgroundColor: "plum", flex: 1, padding: 60 }}>
+      <ScrollView>
+        <Image
+          source={{ uri: "https://picsum.photos/200" }}
+          style={{ width: 200, height: 200 }}
+        />
+        <Text>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum
+          repudiandae corrupti tenetur aut. Error aliquam quas itaque laboriosam
+          alias sapiente porro! Omnis dolorum ab in dolorem incidunt veniam,
+          corporis repudiandae. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Illum repudiandae corrupti tenetur aut. Error
+          aliquam quas itaque laboriosam alias sapiente porro! Omnis dolorum ab
+          in dolorem incidunt veniam, corporis repudiandae. Lorem ipsum dolor
+          sit amet consectetur adipisicing elit. Illum repudiandae corrupti
+          tenetur aut. Error aliquam quas itaque laboriosam alias sapiente
+          porro! Omnis dolorum ab in dolorem incidunt veniam, corporis
+          repudiandae. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Illum repudiandae corrupti tenetur aut. Error aliquam quas itaque
+          laboriosam alias sapiente porro! Omnis dolorum ab in dolorem incidunt
+          veniam, corporis repudiandae. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Illum repudiandae corrupti tenetur aut. Error
+          aliquam quas itaque laboriosam alias sapiente porro! Omnis dolorum ab
+          in dolorem incidunt veniam, corporis repudiandae. Lorem ipsum dolor
+          sit amet consectetur adipisicing elit. Illum repudiandae corrupti
+          tenetur aut. Error aliquam quas itaque laboriosam alias sapiente
+          porro! Omnis dolorum ab in dolorem incidunt veniam, corporis
+          repudiandae.
+        </Text>
+        <Image
+          source={{ uri: "https://picsum.photos/200" }}
+          style={{ width: 200, height: 200 }}
+        />
+      </ScrollView>
+    </View>
   );
 }
