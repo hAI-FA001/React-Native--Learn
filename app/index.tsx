b1471fddf4f2e5067cc1916d0fcba564d245059f
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-16 20:26:44 +0500

    nest Text + style

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,10 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View style={{ backgroundColor: "plum", flex: 1, padding: 60 }}>
-      <Text>Hello, world!</Text>
+      <Text>
+        <Text style={{ color: "green" }}>Hello</Text>,{" "}
+        <Text style={{ color: "blue" }}>world</Text>!
+      </Text>
     </View>
   );
 }
