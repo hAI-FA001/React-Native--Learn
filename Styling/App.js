7a0a863cc768e5e8ced58bad9e884a7cd87375ad
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-16 22:07:02 +0500

    CSS Box Model (Margin, Border, Padding, Content) + props in RN, borderRadius on <Text> only works in Android

--- Styling/App.js
+++ Styling/App.js
@@ -4,7 +4,8 @@ export default App = () => {
   return (
     <View style={styles.container}>
       <View style={[styles.box, styles.lightGreen]}>
-        <Text>Box 1</Text>
+        {/* borderRadius on Text only works on Android */}
+        <Text style={{ backgroundColor: 'red', borderRadius: 5 }}>Box 1</Text>
       </View>
       <View style={[styles.box, styles.lightBlue]}>
         <Text>Box 2</Text>
@@ -16,9 +17,18 @@ export default App = () => {
 export const styles = StyleSheet.create({
   container: { flex: 1, padding: 60, backgroundColor: 'teal' },
   box: {
-    width: 100,
-    height: 100,
-    padding: 10,
+    width: '50%',
+    height: '25%',
+    // padding: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+
+    marginVertical: 10,
+
+    borderWidth: 2,
+    borderColor: 'purple',
+    borderStyle: 'solid',
+    borderRadius: 5,
   },
   lightGreen: {
     backgroundColor: 'lightgreen',
