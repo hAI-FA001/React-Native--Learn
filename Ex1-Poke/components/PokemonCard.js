a9ee45394fe8c6bc91097fe31aa6c530cb94d464
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-17 13:24:16 +0500

    add data, pass as props, render the data + accessibilityLabel

--- Ex1-Poke/components/PokemonCard.js
+++ Ex1-Poke/components/PokemonCard.js
@@ -1,9 +1,30 @@
-import { View, Text, StyleSheet, Platform } from 'react-native'
+import { View, Text, StyleSheet, Platform, Image } from 'react-native'
 
-const PokemonCard = () => {
+const PokemonCard = ({ name, image, type, hp, moves, weaknesses }) => {
   return (
     <View style={styles.card}>
-      <Text>Pokemon Card</Text>
+      <View>
+        <Text>{name}</Text>
+        <Text>{hp}</Text>
+      </View>
+
+      <Image
+        source={image}
+        style={{ width: 30, height: 30 }}
+        accessibilityLabel={`${name} pokemon`}
+      />
+
+      <View>
+        <Text>{type}</Text>
+      </View>
+
+      <View>
+        <Text>Moves: {moves.join(',')}</Text>
+      </View>
+
+      <View>
+        <Text>Weaknesses: {weaknesses.join(',')}</Text>
+      </View>
     </View>
   )
 }
