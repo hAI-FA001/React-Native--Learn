4756ee5aab2e3019b3b41c3a1c314336515035cd
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-17 12:40:57 +0500

    simplify: cleanup / remove Dimensions, useState and useEffect

--- DynamicUI/App.js
+++ DynamicUI/App.js
@@ -1,37 +1,14 @@
-import { useEffect, useState } from 'react'
-import { View, Text, StyleSheet, Dimensions } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 export default App = () => {
-  const [dims, setDims] = useState({ window: Dimensions.get('window') })
-  useEffect(() => {
-    const subscription = Dimensions.addEventListener('change', (window) => {
-      setDims({ window })
-    })
-    return () => subscription?.remove()
-  })
-
-  const { window } = dims
-  const { width, height } = window
-
   return (
     <View style={styles.container}>
-      <View
-        style={[
-          styles.box,
-          {
-            width: winWidth > 500 ? '70%' : '90%',
-            height: winHeight > 600 ? '60%' : '90%',
-          },
-        ]}
-      >
-        <Text style={{ fontSize: width > 500 ? 50 : 24 }}>Hello world</Text>
+      <View style={[styles.box]}>
+        <Text style={{}}>Hello world</Text>
       </View>
     </View>
   )
 }
-
-// const winWidth = Dimensions.get('window').width
-// const winHeight = Dimensions.get('window').height
 
 const styles = StyleSheet.create({
   container: {
@@ -41,13 +18,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   box: {
-    // width: winWidth > 500 ? '70%' : '90%',
-    // height: winHeight > 600 ? '60%' : '90%',
     backgroundColor: 'lightblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  // text: {
-  //   fontSize: winWidth > 500 ? 50 : 24,
-  // },
 })
