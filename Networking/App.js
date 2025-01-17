ce64498e332f7b7b597b477ee6154718747af955
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-17 18:02:01 +0500

    initial setup

--- Networking/App.js
+++ Networking/App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, StatusBar, View } from 'react-native'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+      <View style={styles.listContainer}></View>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f5f5f5',
+    paddingTop: StatusBar.currentHeight,
   },
-});
+
+  listContainer: {
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+})
