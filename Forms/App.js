b8c7d6368b1b179e3d104a4dcfe58f85299080df
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-17 17:40:56 +0500

    KeyboardAvoidingView + behavior=padding + platform-specific offset for iOS

--- Forms/App.js
+++ Forms/App.js
@@ -1,13 +1,30 @@
 import { useState } from 'react'
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native'
 
 export default function App() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
 
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior="padding"
+      keyboardVerticalOffset={Platform.OS === 'android' ? 0 : 100}
+    >
       <View style={styles.form}>
+        <Image
+          source={require('./assets/adaptive-icon.png')}
+          style={styles.image}
+        />
         <Text style={styles.label}>Username</Text>
         <TextInput
           style={styles.input}
@@ -27,7 +44,7 @@ export default function App() {
 
         <Button title="Login" onPress={() => {}} />
       </View>
-    </View>
+    </KeyboardAvoidingView>
   )
 }
 
@@ -63,5 +80,12 @@ const styles = StyleSheet.create({
     marginBottom: 15,
     padding: 10,
     borderRadius: 5,
+  },
+
+  image: {
+    width: 200,
+    height: 400,
+    alignSelf: 'center',
+    marginBottom: 50,
   },
 })
