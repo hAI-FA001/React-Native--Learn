0a782786cb3b7d58ef276df7c4181683a8956fed
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-17 22:56:34 +0500

    setup screens

---
+++ Navigation/screens/AboutScreen.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const AboutScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>About Screen</Text>
+    </View>
+  )
+}
+export default AboutScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+})
---
+++ Navigation/screens/HomeScreen.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home Screen</Text>
+    </View>
+  )
+}
+export default HomeScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+})
