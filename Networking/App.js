dc165f592aa32c9b22f1c4c9b310b8c8f7db4f9e
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-17 18:04:30 +0500

    initial fetch() + set state + "limit" as param

--- Networking/App.js
+++ Networking/App.js
@@ -1,6 +1,16 @@
+import { useState } from 'react'
 import { SafeAreaView, StyleSheet, StatusBar, View } from 'react-native'
 
 export default function App() {
+  const [postList, setPostList] = useState([])
+
+  const fetchData = async (limit = 10) => {
+    const { data } = await fetch(
+      `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
+    )
+    setPostList(data)
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.listContainer}></View>
