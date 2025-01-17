133010eb1da6b6f96ff13d896f4d23d67fd74e79
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-17 18:07:08 +0500

    fetch in useEffect + add/fix response.json() + display titles in FlatList

--- Networking/App.js
+++ Networking/App.js
@@ -1,19 +1,38 @@
-import { useState } from 'react'
-import { SafeAreaView, StyleSheet, StatusBar, View } from 'react-native'
+import { useEffect, useState } from 'react'
+import {
+  SafeAreaView,
+  StyleSheet,
+  StatusBar,
+  View,
+  FlatList,
+  Text,
+} from 'react-native'
 
 export default function App() {
   const [postList, setPostList] = useState([])
 
   const fetchData = async (limit = 10) => {
-    const { data } = await fetch(
+    const resp = await fetch(
       `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
     )
+    const data = await resp.json()
+
     setPostList(data)
+    console.log(data)
   }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.listContainer}></View>
+      <View style={styles.listContainer}>
+        <FlatList
+          data={postList}
+          renderItem={({ item }) => <Text>{item.title}</Text>}
+        />
+      </View>
     </SafeAreaView>
   )
 }
