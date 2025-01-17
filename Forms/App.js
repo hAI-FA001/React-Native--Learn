d77112ab1dca2dcdb3b940757619b1df3ec60576
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-17 17:07:08 +0500

    TextInput + state + value & onChangeText props

--- Forms/App.js
+++ Forms/App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  SafeAreaView,
+  TextInput,
+} from 'react-native'
 
 export default function App() {
+  const [val, setVal] = useState('')
+  console.log(val)
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+      <TextInput style={styles.input} value={val} onChangeText={setVal} />
+      <Text style={styles.text}>You said: {val}</Text>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: StatusBar.currentHeight,
   },
-});
+
+  input: {
+    height: 40,
+    margin: 12,
+    padding: 10,
+    borderWidth: 1,
+  },
+
+  text: { fontSize: 30, padding: 10 },
+})
