03b36ff156a1c8d1719a4739aaabf7eb78419078
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-17 12:39:08 +0500

    fix drawback by useState and useEffect with inline styles for dynamic dimensions

--- DynamicUI/App.js
+++ DynamicUI/App.js
@@ -1,17 +1,37 @@
+import { useEffect, useState } from 'react'
 import { View, Text, StyleSheet, Dimensions } from 'react-native'
 
 export default App = () => {
+  const [dims, setDims] = useState({ window: Dimensions.get('window') })
+  useEffect(() => {
+    const subscription = Dimensions.addEventListener('change', (window) => {
+      setDims({ window })
+    })
+    return () => subscription?.remove()
+  })
+
+  const { window } = dims
+  const { width, height } = window
+
   return (
     <View style={styles.container}>
-      <View style={styles.box}>
-        <Text style={styles.text}>Hello world</Text>
+      <View
+        style={[
+          styles.box,
+          {
+            width: winWidth > 500 ? '70%' : '90%',
+            height: winHeight > 600 ? '60%' : '90%',
+          },
+        ]}
+      >
+        <Text style={{ fontSize: width > 500 ? 50 : 24 }}>Hello world</Text>
       </View>
     </View>
   )
 }
 
-const winWidth = Dimensions.get('window').width
-const winHeight = Dimensions.get('window').height
+// const winWidth = Dimensions.get('window').width
+// const winHeight = Dimensions.get('window').height
 
 const styles = StyleSheet.create({
   container: {
@@ -21,13 +41,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   box: {
-    width: winWidth > 500 ? '70%' : '90%',
-    height: winHeight > 600 ? '60%' : '90%',
+    // width: winWidth > 500 ? '70%' : '90%',
+    // height: winHeight > 600 ? '60%' : '90%',
     backgroundColor: 'lightblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text: {
-    fontSize: winWidth > 500 ? 50 : 24,
-  },
+  // text: {
+  //   fontSize: winWidth > 500 ? 50 : 24,
+  // },
 })
