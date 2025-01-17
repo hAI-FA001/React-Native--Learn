66c78985aa9b9df49ae6e23514e33a49df2acdcb
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-17 22:56:50 +0500

    Native Stack Navigator, component + n ame + initialRouteName props

--- Navigation/App.js
+++ Navigation/App.js
@@ -1,9 +1,20 @@
 import { NavigationContainer } from '@react-navigation/native'
-import { StatusBar } from 'expo-status-bar'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { StyleSheet, Text, View } from 'react-native'
+import HomeScreen from './screens/HomeScreen'
+import AboutScreen from './screens/AboutScreen'
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
-  return <NavigationContainer></NavigationContainer>
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="About">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
