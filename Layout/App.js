9640324e7a3a9139c619ffd887510e95a1ecfc26
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-17 12:09:16 +0500

    relative and absolute position

--- Layout/App.js
+++ Layout/App.js
@@ -7,10 +7,19 @@ export default function App() {
     // "flex: 1" to occupy all space
     // <View style={{ backgroundColor: 'plum', flex: 1 }}></View>
     <View style={styles.container}>
-      <Box style={{ backgroundColor: '#8e9b00', flexGrow: 1 }}>Box 1</Box>
-      <Box style={{ backgroundColor: '#b65d1f', flexGrow: 3 }}>Box 2</Box>
-      <Box style={{ backgroundColor: '#1c4c56' }}>Box 3</Box>
-      <Box style={{ backgroundColor: '#ab9156' }}>Box 4</Box>
+      <Box style={{ backgroundColor: '#8e9b00', top: 75, left: 75 }}>Box 1</Box>
+      <Box style={{ backgroundColor: '#b65d1f' }}>Box 2</Box>
+      <Box style={{ backgroundColor: '#1c4c56', top: 75, left: 75 }}>Box 3</Box>
+      <Box
+        style={{
+          backgroundColor: '#ab9156',
+          top: 100,
+          left: 100,
+          position: 'absolute',
+        }}
+      >
+        Box 4
+      </Box>
       <Box style={{ backgroundColor: '#6b0003' }}>Box 5</Box>
       <Box style={{ backgroundColor: '#1c4c56' }}>Box 6</Box>
       <Box style={{ backgroundColor: '#b95f21' }}>Box 7</Box>
@@ -23,19 +32,6 @@ const styles = StyleSheet.create({
     marginTop: 64,
     borderWidth: 6,
     borderColor: 'red',
-
-    // height: 300,
-    // width: 300,
-
-    flex: 1, // border shows it takes up all space rather than only space occupied by children
-    // flexDirection: 'row',
-    // justifyContent: 'space-between',
-    // alignItems: 'flex-start',
-    // flexWrap: 'wrap',
-    // alignContent: 'center',
-
-    // rowGap: 10,
-    // columnGap: 20,
-    // gap: 20,
+    flex: 1,
   },
 })
