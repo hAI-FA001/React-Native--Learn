f4bed851eb1a4b75666a56439daffff4c408eb38
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-17 17:28:51 +0500

    basic Log-In form

--- Forms/App.js
+++ Forms/App.js
@@ -1,72 +1,57 @@
 import { useState } from 'react'
-import {
-  StyleSheet,
-  Text,
-  View,
-  StatusBar,
-  SafeAreaView,
-  TextInput,
-  Switch,
-} from 'react-native'
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
 
 export default function App() {
-  const [val, setVal] = useState('')
-  const [isDark, setIsDark] = useState(false)
-
   return (
-    <SafeAreaView style={styles.container}>
-      <TextInput
-        style={styles.input}
-        value={val}
-        onChangeText={setVal}
-        placeholder="example@email.com"
-        // secureTextEntry
-        // keyboardType="numeric"
-        autoCorrect={true}
-        autoCapitalize="characters"
-      />
-      <TextInput
-        style={[styles.input, styles.multilineText]}
-        placeholder="message"
-        multiline
-      />
-      <Text style={styles.text}>You said: {val}</Text>
+    <View style={styles.container}>
+      <View style={styles.form}>
+        <Text style={styles.label}>Username</Text>
+        <TextInput style={styles.input} placeholder="Enter username" />
 
-      <View style={styles.switchContainer}>
-        <Text style={styles.text}>Dark Mode</Text>
-        <Switch
-          value={isDark}
-          onValueChange={() => setIsDark(!isDark)}
-          trackColor={{ false: '#767577', true: 'lightblue' }}
-          thumbColor={'#f4f3f4'}
+        <Text style={styles.label}>Password</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Enter password"
+          secureTextEntry
         />
+
+        <Button title="Login" onPress={() => {}} />
       </View>
-    </SafeAreaView>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: StatusBar.currentHeight,
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+    backgroundColor: '#f5f5f5',
   },
 
+  form: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+
+  label: {
+    fontSize: 16,
+    marginBottom: 5,
+    fontWeight: 'bold',
+  },
   input: {
     height: 40,
-    margin: 12,
-    padding: 10,
+    borderColor: '#ddd',
     borderWidth: 1,
-  },
-
-  text: { fontSize: 30, padding: 10 },
-
-  multilineText: { minHeight: 100, textAlignVertical: 'top' },
-
-  switchContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    paddingHorizontal: 10,
+    marginBottom: 15,
+    padding: 10,
+    borderRadius: 5,
   },
 })
