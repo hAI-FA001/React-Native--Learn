b9723265a9e79eb01dbae1fda14581167e39235f
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-18 13:52:25 +0500

    add Settings Screen

---
+++ Navigation/screens/SettingsScreen.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const SettingsScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Settings Screen</Text>
+    </View>
+  )
+}
+export default SettingsScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+})
