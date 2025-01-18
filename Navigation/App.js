43b82f2c5e943cecf9c84510c581d173c2ab57c4
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-18 14:14:18 +0500

    Tab Screen options + Ionicons

--- Navigation/App.js
+++ Navigation/App.js
@@ -4,6 +4,8 @@ import SettingsScreen from './screens/SettingsScreen'
 import ProfileScreen from './screens/ProfileScreen'
 import CourseListScreen from './screens/CourseListScreen'
 
+import Ionicons from '@expo/vector-icons/Ionicons'
+
 const Tab = createBottomTabNavigator()
 
 export default App = () => {
@@ -18,7 +20,17 @@ export default App = () => {
         }}
       >
         <Tab.Screen name="Settings" component={SettingsScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarLabel: 'My Profile',
+            tabBarIcon: ({ color }) => (
+              <Ionicons name="person" size={20} color={color} />
+            ),
+            tabBarBadge: 3,
+          }}
+        />
         <Tab.Screen name="CourseList" component={CourseListScreen} />
       </Tab.Navigator>
     </NavigationContainer>
