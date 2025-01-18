095c22745a324fb4bdd985f59d56591731694571
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-18 13:54:09 +0500

    setup screens in App.js

--- Navigation/App.js
+++ Navigation/App.js
@@ -1,6 +1,8 @@
 import 'react-native-gesture-handler'
 import { NavigationContainer } from '@react-navigation/native'
 import { createDrawerNavigator } from '@react-navigation/drawer'
+import SettingsScreen from './screens/SettingsScreen'
+import DashboardScreen from './screens/DashboardScreen'
 
 const Drawer = createDrawerNavigator()
 
@@ -8,7 +10,8 @@ export default App = () => {
   return (
     <NavigationContainer>
       <Drawer.Navigator>
-        <Drawer.Screen />
+        <Drawer.Screen name="Dashboard" component={DashboardScreen} />
+        <Drawer.Screen name="Settings" component={SettingsScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
   )
