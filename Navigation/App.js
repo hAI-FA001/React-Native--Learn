39e62b92c54cd890ca43ec5e71fc881186db7521
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2025-01-18 13:59:42 +0500

    options for Drawer

--- Navigation/App.js
+++ Navigation/App.js
@@ -10,7 +10,21 @@ export default App = () => {
   return (
     <NavigationContainer>
       <Drawer.Navigator>
-        <Drawer.Screen name="Dashboard" component={DashboardScreen} />
+        <Drawer.Screen
+          name="Dashboard"
+          component={DashboardScreen}
+          options={{
+            title: 'Your Dashboard',
+            drawerLabel: 'Dashboard',
+
+            drawerActiveTintColor: '#333',
+            drawerActiveBackgroundColor: 'lightblue',
+
+            drawerContentStyle: {
+              backgroundColor: '#c6cbef',
+            },
+          }}
+        />
         <Drawer.Screen name="Settings" component={SettingsScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
